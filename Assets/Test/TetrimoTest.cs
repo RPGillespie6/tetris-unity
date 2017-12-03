e926745bb7653f74571e0c4b320a77dd3cb1379c
Bryan Gillespie <rpgillespie6@gmail.com>
2017-12-02 22:11:28 -0500

    Add some Tetrimo  property tests

--- Assets/Test/TetrimoTest.cs
+++ Assets/Test/TetrimoTest.cs
@@ -9,11 +9,6 @@ public class TetrimoTest {
 
     private void setupTetrimo(ref Tetrimo t)
     {
-        // var go = new GameObject();
-        // go.AddComponent<Tetrimo>();
-        // var t = go.GetComponent<Tetrimo>();
-        // setupTetrimo(ref t);
-        // t.State = Tetrimo.TetrimoState.Spawning;
         t.TetrimoPartPrefab      = Resources.Load("TetrimoPartPrefab") as GameObject;
         t.TetrimoPrefab          = Resources.Load("TetrimoPrefab") as GameObject;
         t.TetrimoExplosionPrefab = Resources.Load("TetrimoExplosionPrefab") as GameObject;
@@ -38,6 +33,80 @@ public class TetrimoTest {
                 for (int k = 0; k < blocks_per_rotation; k++) {
                     Assert.AreEqual(shapes[i,j,k], good_shapes[i*blocks_per_rotation*rotations_per_shape + j*blocks_per_rotation + k]);
                 }
+    }
+
+    //Checks if tetrimo can move down based on current position
+    [UnityTest]
+    public IEnumerator TetrimoTest_CanMoveDown()
+    {
+        var go = new GameObject();
+        go.AddComponent<Tetrimo>();
+        var t = go.GetComponent<Tetrimo>();
+        setupTetrimo(ref t);
+        t.State = Tetrimo.TetrimoState.Spawning;
+
+        //Wait for Start() to be called
+        yield return new WaitForEndOfFrame();
+        
+        // Use reflection to access protected fields!
+        FieldInfo IsMovingHorizontal = typeof(Tetrimo).GetField("IsMovingHorizontal", BindingFlags.NonPublic | BindingFlags.Instance);
+        PropertyInfo CanMoveDown = typeof(Tetrimo).GetProperty("CanMoveDown", BindingFlags.NonPublic | BindingFlags.Instance);
+        
+        IsMovingHorizontal.SetValue(t, true);
+        Assert.IsFalse((bool) CanMoveDown.GetValue(t, null));
+
+        IsMovingHorizontal.SetValue(t, false);
+        Assert.IsTrue((bool) CanMoveDown.GetValue(t, null));
+
+        //Move piece to bottom of screen
+        go.transform.Translate(19*Vector3.down, Space.World);
+        Assert.IsFalse((bool) CanMoveDown.GetValue(t, null));
+    }
+
+    //Checks if tetrimo can move down based on current position
+    [UnityTest]
+    public IEnumerator TetrimoTest_CanMoveRight()
+    {
+        var go = new GameObject();
+        go.AddComponent<Tetrimo>();
+        var t = go.GetComponent<Tetrimo>();
+        setupTetrimo(ref t);
+        t.State = Tetrimo.TetrimoState.Spawning;
+
+        //Wait for Start() to be called
+        yield return new WaitForEndOfFrame();
+        
+        // Use reflection to access protected fields!
+        PropertyInfo CanMoveRight = typeof(Tetrimo).GetProperty("CanMoveRight", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        Assert.IsTrue((bool) CanMoveRight.GetValue(t, null));
+
+        //Move piece to right of screen
+        go.transform.Translate(10*Vector3.right, Space.World);
+        Assert.IsFalse((bool) CanMoveRight.GetValue(t, null));
+    }
+
+    //Checks if tetrimo can move down based on current position
+    [UnityTest]
+    public IEnumerator TetrimoTest_CanMoveLeft()
+    {
+        var go = new GameObject();
+        go.AddComponent<Tetrimo>();
+        var t = go.GetComponent<Tetrimo>();
+        setupTetrimo(ref t);
+        t.State = Tetrimo.TetrimoState.Spawning;
+
+        //Wait for Start() to be called
+        yield return new WaitForEndOfFrame();
+        
+        // Use reflection to access protected fields!
+        PropertyInfo CanMoveLeft = typeof(Tetrimo).GetProperty("CanMoveLeft", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        Assert.IsTrue((bool) CanMoveLeft.GetValue(t, null));
+
+        //Move piece to left of screen
+        go.transform.Translate(10*Vector3.left, Space.World);
+        Assert.IsFalse((bool) CanMoveLeft.GetValue(t, null));
     }
 
 }
